81c6ddd518af8d69476f0bc3851254778e3556c3
Joseph-Lukudu-Nelson <jkudus2022@gmail.com>
2022-10-17 12:00:51 -0700

    team task

--- _putchar.c
+++ _putchar.c
@@ -1,14 +1,12 @@
 #include <unistd.h>
 
-
-
 /**
- * _putchar - writes c character in the stdout
- * @c: its the character to be printed
- * Return: On success 1
- * On error, return -1 and set error appropriately
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
